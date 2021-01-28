ddeb0146f769e4e7088bb808df5c6b3663efd2e1
LukeBouch <lukebouch2014@gmail.com>
2021-01-28 15:26:17 -0500

    Added URL rewriting

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ include 'vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader('static/templates/');
 $twig = new \Twig\Environment($loader, []);
 
-
-echo $twig->render('home.html');
+if ($_GET['q'] == 'curriculum') {
+    echo $twig->render('curriculum.html');
+} else {
+    echo $twig->render('home.html');
+}
 ?>
