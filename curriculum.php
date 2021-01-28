503b865b3e89312d6b4f1435af8796eb7a7b20a2
LukeBouch <lukebouch2014@gmail.com>
2021-01-28 15:36:30 -0500

    Delete curriculum.php

--- curriculum.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'vendor/autoload.php';
-
-$loader = new \Twig\Loader\FilesystemLoader('static/templates/');
-$twig = new \Twig\Environment($loader, []);
-
-
-echo $twig->render('curriculum.html');
-?>
