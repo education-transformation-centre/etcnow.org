1c29a97c662c56378d3ac7c738d2c71052a41b17
LukeBouch <lukebouch2014@gmail.com>
2021-02-08 11:18:50 -0500

    Created 404 page

--- index.php
+++ index.php
@@ -4,9 +4,11 @@ include 'vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader('static/templates/');
 $twig = new \Twig\Environment($loader, []);
 
-if ($_GET['q'] == 'curriculum') {
+if (!isset($_GET['q'])) {
+    echo $twig->render('home.html');
+} if ($_GET['q'] == 'curriculum') {
     echo $twig->render('curriculum.html');
 } else {
-    echo $twig->render('home.html');
+    echo $twig->render('404.html');
 }
 ?>
