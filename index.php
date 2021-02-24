19bfa059d625732e56b3578a66bc2412ee76e4c4
LukeBouch <lukebouch2014@gmail.com>
2021-02-23 20:15:23 -0500

    Merged Curriculum page to Home page

--- index.php
+++ index.php
@@ -6,8 +6,6 @@ $twig = new \Twig\Environment($loader, []);
 
 if (!isset($_GET['q'])) {
     echo $twig->render('home.html');
-} else if ($_GET['q'] == 'curriculum') {
-    echo $twig->render('curriculum.html');
 } else {
     echo $twig->render('404.html');
 }
