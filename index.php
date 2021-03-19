dcc1913883f35ae278b574d33d475cdcb80f1947
LukeBouch <lukebouch2014@gmail.com>
2021-03-19 12:32:35 -0400

    Improved Cache Busting

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 include 'vendor/autoload.php';
 
+$data = [
+    $version = '20210319-01'
+];
+
 $loader = new \Twig\Loader\FilesystemLoader('static/templates/');
 $twig = new \Twig\Environment($loader, []);
 
 if (!isset($_GET['q'])) {
-    echo $twig->render('home.html');
+    echo $twig->render('home.html', $data);
 } else {
-    echo $twig->render('404.html');
+    echo $twig->render('404.html', $data);
 }
 ?>
