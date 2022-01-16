d1ea81a2271cf398fc08fe4643ab15ae0648a856
Luke Bouch <lukebouch2014@gmail.com>
2022-01-16 11:35:51 -0500

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -215,21 +215,21 @@
             </x-testimony>
             <x-testimony name="Shelley Faulkner M.A., Ed.S." title="Trauma Counselor" subtitle="ReadyKids">
                 <p>
-                    Dr. Carr was a steadfast presence in my client’s life which I feel helped her to
-                    see more positive possibilities for her future. Dr. Carr provided my
-                    client with individualized and focused support that she needed during a challenging
-                    time in her life. This extra support helped to scaffold my client’s sense of
-                    what a trustworthy relationship looks and feels like.
+                    Dr. Carr was a steadfast presence in my client's life which I feel helped her to
+                    see more positive possibilities for her future. Dr. Carr provided my
+                    client with individualized and focused support that she needed during a challenging
+                    time in her life. This extra support helped to scaffold my client's sense of
+                    what a trustworthy relationship looks and feels like.
                 </p>
                 <p>
                     I believe that putting in the extra time and effort often help children/students to feel
                     that they really do matter and that their life is meaningful and worthwhile. Being a
                     constant, honest and present adult, helps to create trust in the
-                    relationship between adult and child. Young people need to learn what a safe
+                    relationship between adult and child. Young people need to learn what a safe
                     and trusting relationship and I was able to witness Dr. Carr be that person for my
-                    client.  Dr. Carr's guidance through the positive seeds planted by the Education
-                    Transformation Centre, will have lasting effects on my client's life,
-                    whether she sees/feels it now or in her future.
+                    client. Dr. Carr's guidance through the positive seeds planted by the Education
+                    Transformation Centre, will have lasting effects on my client's life,
+                    whether she sees/feels it now or in her future.
                 </p>
                 <p>
                     All the best Dr. Carr and I hope to work with you again very soon. I love what you are
