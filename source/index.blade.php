065902ad37f61daaa2e9d5b5595183873a324c86
Luke Bouch <lukebouch2014@gmail.com>
2022-01-28 16:02:37 -0500

    Add funding sources

--- source/index.blade.php
+++ source/index.blade.php
@@ -105,6 +105,18 @@
         </div>
         <img class="max-w-xl w-full mx-auto mt-10 rounded-lg shadow-lg" src="assets/images/outcomes.jpg"
             alt="Group of students graduating" class="rounded small center" id="outcomes-img">
+        <section class="py-10 my-10">
+            <div class="container text-center">
+                <h2>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 mb-2 mx-auto" fill="none"
+                        viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>Funding Sources
+                </h2>
+                <p>We accept CSA, Private Insurance, and Direct Payments.</p>
+            </div>
+        </section>
     </div>
 </section>
 
