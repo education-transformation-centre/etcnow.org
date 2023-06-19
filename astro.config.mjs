f0d2d53c9d479488324b96ab09e5bc4daa227319
Luke Bouch <luke@lukebouch.com>
2023-06-19 12:32:59 -0400

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://etcnow.org',
-  integrations: [tailwind()]
+  site: "https://etcnow.org",
+  integrations: [tailwind()],
 });
