6725f2f7c5702cc1a3c36293a67785e7bd832b13
Luke Bouch <luke@lukebouch.com>
2023-06-19 13:39:57 -0400

    Migration to Astro (#14)
    * Add .gitignore for Astro
    
    * Start fresh with Astro
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Add images
    
    * Remove empty class attributes
    
    * Format css
    
    * Fix
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Update node modules
    
    * Fix tailwindcss issues
    
    * WIP
    
    * Fix
    
    * Add quote component
    
    * Add quote component
    
    * Fix
    
    * Install AlpineJS Astro integration
    
    * Add contact confirmation page

--- source/_components/quote.blade.php
+++
@@ -1,9 +0,0 @@
-<blockquote class="text-gray-800">
-    <p class="mb-0 italic">
-        "{{ $slot }}"
-    </p>
-    -
-    <cite class="mt-0 font-semibold">
-        {{ $cite }}
-    </cite>
-</blockquote>
