91199d31043c16414130fd49069eea59bbd53b00
Luke Bouch <luke@lukebouch.com>
2025-01-24 18:49:39 -0500

    WIP

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,14 +7,6 @@ module.exports = {
                 sans: ["Metropolis", "sans-serif"],
                 serif: ["Merriweather", "sans-serif"],
             },
-            fontSize: {
-                lg: "1.25rem",
-                xl: "1.563rem",
-                "2xl": "1.953rem",
-                "3xl": "2.441rem",
-                "4xl": "3.052rem",
-                "5xl": "3.815rem",
-            },
             colors: {
                 brown: {
                     dark: "#6c4d40",
